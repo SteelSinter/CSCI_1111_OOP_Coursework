0ff145bf064a7cc69d8f48586970792da6a4f8a8
James <145038163+SteelSinter@users.noreply.github.com>
2023-10-25 10:54:56 -0600

    Add files via upload

---
+++ Chapter10/DesigningClassesRealWorld.java
@@ -0,0 +1,117 @@
+package objects;
+
+public class DesigningClassesRealWorld {
+
+	public static void main(String[] args) {
+		char[] ch = {'1', '2', '6', '4', '7', '9'};
+		
+		MyInteger myInt = new MyInteger(5);
+		
+		int chAsInt = myInt.parseInt(ch);
+		
+		System.out.println(chAsInt);
+		System.out.println(myInt.value);
+
+	}
+
+}
+
+class MyInteger {
+	int value;
+	
+	MyInteger(int i) {
+		value = i;
+	}
+	
+	int getInt() {
+		return value;
+	}
+	
+	boolean isEven() {
+		if(value % 2 == 0)
+			return true;
+		else
+			return false;
+	}
+	
+	boolean isOdd() {
+		if(!(value % 2 == 0))
+			return true;
+		else
+			return false;
+	}
+	
+	boolean isPrime() {
+		if(value % value != 1 && value % 1 == value)
+			return false;
+		else
+			return true;
+	}
+	
+	static boolean isEven(int i) {
+		if(i % 2 == 0)
+			return true;
+		else
+			return false;
+	}
+	
+	static boolean isOdd(int i) {
+		if(!(i % 2 == 0))
+			return true;
+		else
+			return false;
+	}
+	
+	static boolean isPrime(int i) {
+		if(i % i != 1 && i % 1 == i)
+			return false;
+		else
+			return true;
+	}
+	
+	static boolean isEven(MyInteger i) {
+		if(i.value % 2 == 0)
+			return true;
+		else
+			return false;
+	}
+	
+	static boolean isOdd(MyInteger i) {
+		if(!(i.value % 2 == 0))
+			return true;
+		else
+			return false;
+	}
+	
+	static boolean isPrime(MyInteger i) {
+		if(i.value % i.value != 1 && i.value % 1 == i.value)
+			return false;
+		else
+			return true;
+	}
+	
+	boolean equals(int i) {
+		if(i == value)
+			return true;
+		else
+			return false;
+	}
+	
+	boolean equals(MyInteger i) {
+		if(i.value == value)
+			return true;
+		else
+			return false;
+	}
+	
+	static int parseInt(char[] ch) {
+		int result = Integer.parseInt(new String(ch));
+		
+		return result;
+	}
+	
+	static int parseInt(String str) {
+		return Integer.parseInt(str);
+	}
+	
+}

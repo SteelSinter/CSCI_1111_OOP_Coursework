6bb20826bf0ea4f97de9c0c8ce422056bdc9a30b
James <145038163+SteelSinter@users.noreply.github.com>
2023-10-30 13:28:15 -0600

    Update InheritanceFundamental.java

--- Chapter11/InheritanceFundamental.java
+++ Chapter11/InheritanceFundamental.java
@@ -1,10 +1,8 @@
 package objects;
-import java.util.Scanner;
 
 public class InheritanceFundamental {
 
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
 		
 		
 
